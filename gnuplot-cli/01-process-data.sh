0903e91cf383b24fe0d164d53c4dd506f725b7a6
Christopher S. Simmons <csim@mghpcc.org>
2023-03-21 21:40:09 -0400

    adding return line via echo for readability

--- gnuplot-cli/01-process-data.sh
+++ gnuplot-cli/01-process-data.sh
@@ -4,7 +4,7 @@ echo "running gnuplot using the anscombe.gnu script"
 echo " $ gnuplot < anscombe.gnu"
 
 gnuplot < anscombe.gnu
-
+echo ""
 ret=$?
 if [ $ret -ne 0 ]; then
         echo "Data processing failed ... HALP "
